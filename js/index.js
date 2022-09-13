df2f9a328f78d94daece3227e1ab7186fa317e79
yousef <youseff2000q@gmail.com>
2022-09-13 07:44:00 -0700

    edit gallery and make it shorter

--- js/index.js
+++ js/index.js
@@ -34,43 +34,42 @@ show.addEventListener("click", (e) => {
     optionToggle = !optionToggle;
     show.innerHTML = "hide";
   } else {
-    option.style.left = "-25%";
+    option.style.left = "-200px";
     optionToggle = !optionToggle;
     show.innerHTML = "show";
   }
 });
 //
 // loacal storage
-let localItem = localStorage.getItem("ccolor1");
-console.log(localItem);
+let localItem = sessionStorage.getItem("ccolor1");
 if (localItem !== null) {
   document.documentElement.style.setProperty(
     "--dark-color",
-    localStorage.getItem("ccolor1")
+    sessionStorage.getItem("ccolor1")
   );
   document.documentElement.style.setProperty(
     "--icons-color",
-    localStorage.getItem("ccolor2")
+    sessionStorage.getItem("ccolor2")
   );
   document.documentElement.style.setProperty(
     "--roundedLink-color",
-    localStorage.getItem("ccolor3")
+    sessionStorage.getItem("ccolor3")
   );
   document.documentElement.style.setProperty(
     "--linkText-color",
-    localStorage.getItem("ccolor4")
+    sessionStorage.getItem("ccolor4")
   );
   document.documentElement.style.setProperty(
     "--section-color",
-    localStorage.getItem("ccolor5")
+    sessionStorage.getItem("ccolor5")
   );
   document.documentElement.style.setProperty(
     "--theme-text",
-    localStorage.getItem("ccolor6")
+    sessionStorage.getItem("ccolor6")
   );
   document.documentElement.style.setProperty(
     "--mix-section",
-    localStorage.getItem("ccolor7")
+    sessionStorage.getItem("ccolor7")
   );
 }
 spans.forEach((span) => {
@@ -79,18 +78,13 @@ spans.forEach((span) => {
       span.classList.remove("spanActive");
     });
     // set item
-    console.log(e.target.dataset.ccolor1);
-    console.log(e.target.dataset.ccolor2);
-    console.log(e.target.dataset.ccolor3);
-    console.log(e.target.dataset.ccolor4);
-    console.log(e.target.dataset.ccolor5);
-    localStorage.setItem("ccolor2", e.target.dataset.ccolor2);
-    localStorage.setItem("ccolor1", e.target.dataset.ccolor1);
-    localStorage.setItem("ccolor3", e.target.dataset.ccolor3);
-    localStorage.setItem("ccolor4", e.target.dataset.ccolor4);
-    localStorage.setItem("ccolor5", e.target.dataset.ccolor5);
-    localStorage.setItem("ccolor6", e.target.dataset.ccolor6);
-    localStorage.setItem("ccolor7", e.target.dataset.ccolor7);
+    sessionStorage.setItem("ccolor2", e.target.dataset.ccolor2);
+    sessionStorage.setItem("ccolor1", e.target.dataset.ccolor1);
+    sessionStorage.setItem("ccolor3", e.target.dataset.ccolor3);
+    sessionStorage.setItem("ccolor4", e.target.dataset.ccolor4);
+    sessionStorage.setItem("ccolor5", e.target.dataset.ccolor5);
+    sessionStorage.setItem("ccolor6", e.target.dataset.ccolor6);
+    sessionStorage.setItem("ccolor7", e.target.dataset.ccolor7);
     // set item
     e.target.classList.add("spanActive");
     //
@@ -186,36 +180,9 @@ myLinks.forEach((link) => {
 // start portfolio
 const lis = document.querySelectorAll(".portfolio ul li"),
   images = document.querySelectorAll(".portfolio .all"),
-  imgs = document.querySelectorAll(".portfolio .all img");
-// overLay = document.querySelectorAll(".overLay");
-
-// imgs.forEach((img) => {
-//   // console.log(img);
-//   img.addEventListener("click", (e) => {
-//     // console.log(img);
-//     // overlay//
-//     let over = document.createElement("div");
-//     over.className = "overLay";
-//     document.body.prepend(over);
-//     //  popup
-//     let popup = document.createElement("img");
-//     console.log(e.target.src);
-//     popup.setAttribute("src", e.target.src);
-
-//     popup.className = "imgOver";
-//     over.prepend(popup);
-//     //
-//     let span = document.createElement("span");
-//     let text = document.createTextNode("X");
-//     span.style.cssText =
-//       "position: absolute;z-index: 5002;   background: #111;  color: white; left: 50%; top: 20%;padding: 10px;border-radius: 50%; cursor:pointer;";
-//     span.appendChild(text);
-//     over.append(span);
-//     span.addEventListener("click", () => {
-//       over.remove();
-//     });
-//   });
-// });
+  imgs = document.querySelectorAll(".portfolio .all img"),
+  gallery = document.querySelectorAll(".portfolio .gallery");
+console.log(gallery);
 imgs.forEach((image) => {
   image.addEventListener("click", (e) => {
     // console.log(e.target)
@@ -256,52 +223,13 @@ lis.forEach((li) => {
       li.classList.remove("active");
     });
     element.target.classList.add("active");
-    console.log(element.target.classList.contains("design"));
-    if (element.target.classList.contains("design")) {
-      images.forEach((image) => {
-        if (image.classList.contains("design")) {
-          image.style.cssText = "display:block;";
-        } else {
-          image.style.cssText = "display:none;";
-        }
-      });
-    }
-    if (element.target.classList.contains("code")) {
-      images.forEach((image) => {
-        if (image.classList.contains("code")) {
-          image.style.cssText = "display:block;";
-        } else {
-          image.style.cssText = "display:none;";
-        }
-      });
-    }
-    if (element.target.classList.contains("photo")) {
-      images.forEach((image) => {
-        if (image.classList.contains("photo")) {
-          image.style.cssText = "display:block;";
-        } else {
-          image.style.cssText = "display:none;";
-        }
-      });
-    }
-    if (element.target.classList.contains("app")) {
-      images.forEach((image) => {
-        if (image.classList.contains("app")) {
-          image.style.cssText = "display:block;";
-        } else {
-          image.style.cssText = "display:none;";
-        }
-      });
-    }
-    if (element.target.classList.contains("All")) {
-      images.forEach((image) => {
-        if (image.classList.contains("all")) {
-          image.style.cssText = "display:block;";
-        } else {
-          image.style.cssText = "display:none;";
-        }
-      });
-    }
+    gallery.forEach((item) => {
+      item.style.cssText = "transform:scale(0);width:0;padding:0;";
+    });
+
+    document.querySelectorAll(element.target.dataset.type).forEach((item) => {
+      item.style.cssText = "transform:scale(1);";
+    });
   });
 });
 // end portfolio
